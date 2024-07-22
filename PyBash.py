f698cee48b4be9dca7233522055cc19913426b93
Inferno265 <151403860+Inferno265@users.noreply.github.com>
2024-07-22 21:05:26 +0200

    Update PyBash.py
    Add commands:
    
    cat - Reads from files
    touch - Makes files
    rm - Removes files/dirs

--- PyBash.py
+++ PyBash.py
@@ -7,7 +7,7 @@ def get_modified_cwd():
 os.system('color 0a')
 while True:
     modified_cwd = get_modified_cwd()
-    x = input(modified_cwd + '~ ')
+    x = input(modified_cwd + '~$ ')
     
     if x == 'exit':
         os.system('color 0f')
@@ -24,6 +24,42 @@ while True:
             print(f"Error changing directory: {e}")
     elif x == 'version':
         print('v1.0.0 Copyright © 2024 Inferno')
+    elif x.startswith('cat '):
+        new_x = x.split(' ')
+        try:
+            if new_x[1] == '-n':
+                with open(new_x[2]) as file:
+                    print(file.read())
+        except (IndexError, FileNotFoundError):
+            print('Usage: cat [-option] [file to read]')
+    elif x.startswith('touch '):
+        new_x = x.split(' ')
+        try:
+            f = open(new_x[1], 'a')
+            f.close()
+        except IndexError:
+            print('Usage: touch [file]')
+        except OSError:
+            print('An unexpected error has occurred')
+    elif x.startswith('rm '):
+        new_x = x.split(' ')
+        rmcm = ['-r', '-f']
+        try:
+            if new_x[1] in rmcm:
+                if new_x[1] == '-f':
+                    try:
+                        os.remove(new_x[2])
+                    except OSError:
+                        print('Warning! Critical Error. DO NOT ATTEMPT TO DELETE AGAIN. If you are sure to do it, delete it using sudo rm [file].')
+            else:
+                if os.path.exists(new_x[1]):
+                    os.remove(new_x[1])
+                else:
+                    print('File not found')
+        except IndexError:
+            print('Usage: rm [-option] [file]')
+    elif x.startswith('sudo '):
+        print('Working on $ sudo!')
     else: 
         print("Unknown Command Detected.")
 
